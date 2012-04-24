d5c1476cdeda9a8e11e64c2e2fa6b8a50c6c514c
ematheson <ematheson@davenport.edu>
2012-04-24 16:38:34 -0400

    Update theme settings

--- README.txt
+++ README.txt
@@ -28,12 +28,12 @@ installed in sites/all/themes/, but any valid theme directory is acceptable
 
  1. Setup the location for your new sub-theme.
 
-    Copy the STARTERKIT folder out of the zen/ folder and rename it to be your
+    Copy the tbs folder out of the zen/ folder and rename it to be your
     new sub-theme. IMPORTANT: The name of your sub-theme must start with an
     alphabetic character and can only contain lowercase letters, numbers and
     underscores.
 
-    For example, copy the sites/all/themes/zen/STARTERKIT folder and rename it
+    For example, copy the sites/all/themes/zen/tbs folder and rename it
     as sites/all/themes/foo.
 
       Why? Each theme should reside in its own folder. To make it easier to
@@ -42,11 +42,11 @@ installed in sites/all/themes/, but any valid theme directory is acceptable
 
  2. Setup the basic information for your sub-theme.
 
-    In your new sub-theme folder, rename the STARTERKIT.info.txt file to include
+    In your new sub-theme folder, rename the tbs.info.txt file to include
     the name of your new sub-theme and remove the ".txt" extension. Then edit
     the .info file by editing the name and description field.
 
-    For example, rename the foo/STARTERKIT.info file to foo/foo.info. Edit the
+    For example, rename the foo/tbs.info file to foo/foo.info. Edit the
     foo.info file and change "name = Zen Sub-theme Starter Kit" to "name = Foo"
     and "description = Read..." to "description = A Zen sub-theme".
 
@@ -85,11 +85,11 @@ installed in sites/all/themes/, but any valid theme directory is acceptable
  4. Edit your sub-theme to use the proper function names.
 
     Edit the template.php and theme-settings.php files in your sub-theme's
-    folder; replace ALL occurrences of "STARTERKIT" with the name of your
+    folder; replace ALL occurrences of "tbs" with the name of your
     sub-theme.
 
     For example, edit foo/template.php and foo/theme-settings.php and replace
-    every occurrence of "STARTERKIT" with "foo".
+    every occurrence of "tbs" with "foo".
 
     It is recommended to use a text editing application with search and
     "replace all" functionality.
--- template.php
+++ template.php
@@ -60,12 +60,12 @@
  *   api.drupal.org website is a good place to find which file contains which
  *   function.) Then you can copy the original function in its entirety and
  *   paste it in this template.php file, changing the prefix from theme_ to
- *   STARTERKIT_. For example:
+ *   tbs_. For example:
  *
  *     original, found in modules/field/field.module: theme_field()
- *     theme override, found in template.php: STARTERKIT_field()
+ *     theme override, found in template.php: tbs_field()
  *
- *   where STARTERKIT is the name of your sub-theme. For example, the
+ *   where tbs is the name of your sub-theme. For example, the
  *   zen_classic theme would define a zen_classic_field() function.
  *
  *   Note that base themes can also override theme functions. And those
@@ -111,12 +111,12 @@
  *   The name of the template being rendered ("maintenance_page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_maintenance_page(&$variables, $hook) {
+function tbs_preprocess_maintenance_page(&$variables, $hook) {
   // When a variable is manipulated or added in preprocess_html or
   // preprocess_page, that same work is probably needed for the maintenance page
   // as well, so we can just re-use those functions to do that work here.
-  STARTERKIT_preprocess_html($variables, $hook);
-  STARTERKIT_preprocess_page($variables, $hook);
+  tbs_preprocess_html($variables, $hook);
+  tbs_preprocess_page($variables, $hook);
 }
 // */
 
@@ -129,7 +129,7 @@ function STARTERKIT_preprocess_maintenance_page(&$variables, $hook) {
  *   The name of the template being rendered ("html" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_html(&$variables, $hook) {
+function tbs_preprocess_html(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 
   // The body tag's classes are controlled by the $classes_array variable. To
@@ -147,7 +147,7 @@ function STARTERKIT_preprocess_html(&$variables, $hook) {
  *   The name of the template being rendered ("page" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_page(&$variables, $hook) {
+function tbs_preprocess_page(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 }
 // */
@@ -161,11 +161,11 @@ function STARTERKIT_preprocess_page(&$variables, $hook) {
  *   The name of the template being rendered ("node" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_node(&$variables, $hook) {
+function tbs_preprocess_node(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 
   // Optionally, run node-type-specific preprocess functions, like
-  // STARTERKIT_preprocess_node_page() or STARTERKIT_preprocess_node_story().
+  // tbs_preprocess_node_page() or tbs_preprocess_node_story().
   $function = __FUNCTION__ . '_' . $variables['node']->type;
   if (function_exists($function)) {
     $function($variables, $hook);
@@ -182,7 +182,7 @@ function STARTERKIT_preprocess_node(&$variables, $hook) {
  *   The name of the template being rendered ("comment" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_comment(&$variables, $hook) {
+function tbs_preprocess_comment(&$variables, $hook) {
   $variables['sample_variable'] = t('Lorem ipsum.');
 }
 // */
@@ -196,7 +196,7 @@ function STARTERKIT_preprocess_comment(&$variables, $hook) {
  *   The name of the template being rendered ("region" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_region(&$variables, $hook) {
+function tbs_preprocess_region(&$variables, $hook) {
   // Don't use Zen's region--sidebar.tpl.php template for sidebars.
   //if (strpos($variables['region'], 'sidebar_') === 0) {
   //  $variables['theme_hook_suggestions'] = array_diff($variables['theme_hook_suggestions'], array('region__sidebar'));
@@ -213,7 +213,7 @@ function STARTERKIT_preprocess_region(&$variables, $hook) {
  *   The name of the template being rendered ("block" in this case.)
  */
 /* -- Delete this line if you want to use this function
-function STARTERKIT_preprocess_block(&$variables, $hook) {
+function tbs_preprocess_block(&$variables, $hook) {
   // Add a count to all the blocks in the region.
   // $variables['classes_array'][] = 'count-' . $variables['block_id'];
 
